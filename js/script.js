3d1cfee4953fc027021f7a8c16260b99f2a54211
callmejogi <kutafin01@gmail.com>
2021-10-09 12:47:21 +0300

    done

---
+++ js/script.js
@@ -0,0 +1,96 @@
+//#1
+
+//let a = +prompt(`Введите число:`);
+//const result = (a === 0) ? alert(`Верно`): alert(`Неверно`);
+
+//#2
+
+//let a1 = +prompt(`Введите число:`);
+//const result1 = (a1 > 0) ? alert(`Верно`): alert(`Неверно`);
+
+//#3
+
+//let a2 = +prompt(`Введите число:`);
+//const result2 = (a2 < 0) ? alert(`Верно`): alert(`Неверно`);
+
+//#4
+
+//let a3 = +prompt(`Введите число:`);
+//const result3 = (a3 >= 0) ? alert(`Верно`): alert(`Неверно`);
+
+//#5
+
+//let a4 = +prompt(`Введите число:`);
+//const result4 = (a4 <= 0) ? alert(`Верно`): alert(`Неверно`);
+
+//#6
+
+//let a5 = +prompt(`Введите число:`);
+//const result5 = (a5 !== 0) ? alert(`Верно`): alert(`Неверно`);
+
+//#7
+
+//let a6 = prompt(`Введите число:`);
+//const result6 = (a6 === `test`) ? alert(`Верно`): alert(`Неверно`);
+
+//#8
+
+//let a7 = prompt(`Введите число:`);
+//const result7 = (a7 === `1`) ? alert(`Верно`): alert(`Неверно`);
+
+//#9
+
+//let a8 = +prompt(`Введите число:`);
+//const result8 = (a8 < 5 && a8 > 0) ? alert(`Верно`): alert(`Неверно`);
+
+//#10
+//let a9 = +prompt(`Введите число:`);
+
+//if (a9 === 0 || a9 === 2){
+ //   alert(a9+7);
+//}else{
+ //   alert(a9 / 10);
+//}
+
+//#11
+
+//let a10 = +prompt(`Type value of a`);
+//let b = +prompt(`Type value of b`);
+
+//if (a10<=1 && b>=3){
+//    alert(a10+b);
+//}else{
+//    alert(a10-b);
+//}
+
+//#12
+
+//let a11 = +prompt(`Type value of a`);
+//let b1 = +prompt(`Type value of b`);
+
+//if (a11>2 && a11<11 || b1>=6 && b1<14){
+ //   alert(`True`);
+//}else{
+//    alert(`False`);
+//}
+
+//#13
+let num = +prompt(`Type a number:`)
+switch(num) {
+    case 1:
+        result9 = alert(`зима`);
+        break;
+    case 2:
+        result10 = alert(`весна`);
+        break;
+    case 3:
+        result11 = alert(`лето`);
+        break;
+    case 4:
+        result12 = alert(`осень`);
+        break;
+    default:
+        result13 = alert(`пшел вон`)
+}
+
+
